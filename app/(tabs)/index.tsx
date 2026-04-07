bc06ca993fa9361c369a2e81c389a3c5d8eacf8a
Ishak Shekh <shekhishak90@gmail.com>
2026-04-07 11:14:19 +0530

    feat: add custom fonts and update layout components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,9 +9,8 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1  bg-background p-5">
-      <Text className="text-4xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-7xl font-sans-extrabold ">Home</Text>
+      <Text className="text-7xl font-bold  ">Home</Text>
       <Link
         href={"/onboarding"}
         className="mt-4 rounded bg-primary text-white p-4 font-bold text-lg"
@@ -19,10 +18,16 @@ export default function App() {
         Get Started
       </Link>
 
-      <Link href={"/(auth)/signup"} className="mt-4 text-blue-500">
+      <Link
+        href={"/(auth)/signup"}
+        className="mt-4 font-sans-bold text-blue-500"
+      >
         {"Don't"} have an account? Sign up
       </Link>
-      <Link href={"/(auth)/signin"} className="mt-2 text-blue-500">
+      <Link
+        href={"/(auth)/signin"}
+        className="mt-2 font-sans-light text-blue-500"
+      >
         Already have an account? Sign in
       </Link>
 
